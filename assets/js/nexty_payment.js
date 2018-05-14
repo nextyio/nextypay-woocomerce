b9c21b11f0779490b0df6441c35532c78bd78aaa
bestboyvn87 <39051668+bestboyvn87@users.noreply.github.com>
2018-05-14 11:27:59 +0700

    Add files via upload
    ajax function, fix bug to show invalid links in admin settings

--- assets/js/nexty_payment.js
+++ assets/js/nexty_payment.js
@@ -8,20 +8,70 @@ function validURL(str) {
   return pattern.test(str);
 }
 
+function countInSecond(startTime,endTime) {
+  var timeDiff = endTime - startTime; //in ms
+  // strip the ms
+  timeDiff /= 1000;
+
+  // get seconds 
+  var seconds = Math.round(timeDiff);
+  return seconds;
+}
+
+//check paid sum and order status with ajax, in PHP ajax_action()
+function call_ajax(startTime,order_total,order_id,timeout,interval){
+	var seconds=countInSecond(startTime,new Date());
+	console.log(seconds);
+	if (seconds>timeout) {
+		console.log("time out");
+		return;
+	}
+	var paid="0";
+	setTimeout(function(){
+		// This does the ajax request
+		jQuery.ajax({
+			url : my_ajax_object.ajaxurl,
+			type: 'POST',
+		data : {
+			'action' : 'my_action',
+			'order_id' : order_id,
+			'order_total': order_total,
+		}
+		}).done(function ( response ) {
+			console.log(response);
+			paid=response;
+			if (paid[0]=="0") { return call_ajax(startTime,order_total,order_id,timeout,interval); } else 
+			{
+				console.log(response);
+				//alert(response);
+				var current_page= window.location.href;
+				if (paid[0]=='1') window.location = current_page;
+				//alert (test);
+				return;
+			}
+		}).fail(function ( err ) {
+
+		})
+	}, interval*1000);
+}
 
 jQuery(function($) {
+	
 	$(".notice").hide(); //Hide Admin Notice
 	
-	$(".valid_url").on( 'change', function( event ){
-		var address = this.value;
-		if (!validURL(address)) {
-			$(".notice").show();
-		} 
-		else $(".notice").hide();
-	});
+	var valid_endPointAddress=validURL($("#woocommerce_custom_endPointAddress").val());
+	var valid_exchangeAPI=validURL($("#woocommerce_custom_exchangeAPI").val());
+
+	if (valid_endPointAddress && valid_exchangeAPI) {$(".notice").hide();} else $(".notice").show();
+
+	$("#woocommerce_custom_endPointAddress").on( 'change', function( event ){
+		valid_endPointAddress=validURL(this.value);
+		if (valid_endPointAddress && valid_exchangeAPI) {$(".notice").hide();} else $(".notice").show();
+	});	
 	
-	jQuery("form.woocommerce-checkout").on('submit', function(event) {
-        //alert("submiting");
-		//event.preventDefault();
-    });
-});
+	$("#woocommerce_custom_exchangeAPI").on( 'change', function( event ){
+		valid_exchangeAPI=validURL(this.value);
+		if (valid_endPointAddress && valid_exchangeAPI) {$(".notice").hide();} else $(".notice").show();
+	});	
+	
+})
