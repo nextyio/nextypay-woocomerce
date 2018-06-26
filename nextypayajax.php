86c75059ee6d59e658d768e5a81794d38ad3a8d9
bestboyvn87 <39051668+bestboyvn87@users.noreply.github.com>
2018-06-26 10:01:58 +0700

    load 30 blocks each request, so cronjob every minute

--- nextypayajax.php
+++ nextypayajax.php
@@ -1,12 +1,11 @@
 <?php
-    require_once(dirname( dirname(dirname( dirname(__FILE__) )))  . '/wp-config.php');
-    $wp->init();
-    $wp->parse_request();
-    $wp->query_posts();
-    $wp->register_globals();
-    $wp->send_headers();
 
-    // Your Wordpress Functions here...
+require_once(dirname( dirname(dirname( dirname(__FILE__) )))  . '/wp-config.php');
+$wp->init();
+$wp->parse_request();
+$wp->query_posts();
+$wp->register_globals();
+$wp->send_headers();
 
 $nextypay_url			= dirname(__FILE__)."/";
 $nextypay_js_url	= $nextypay_url.'assets/js/';
@@ -33,18 +32,8 @@ $_updatedb->set_url($_nextypay_obj->url);
 $_updatedb->set_connection($wpdb);
 $_updatedb->set_includes($_blockchain,$_functions);
 $_updatedb->set_backend_settings($_db_prefix,$_nextypay_obj->store_currency_code,$_nextypay_obj->walletAddress,
-       $_SERVER['HTTP_HOST'],$_nextypay_obj->min_blocks_saved_db,$_nextypay_obj->max_blocks_saved_db,20);
+       $_SERVER['HTTP_HOST'],$_nextypay_obj->min_blocks_saved_db,$_nextypay_obj->max_blocks_saved_db,30);
 
-$_updatedb->updatedb();
-sleep(9);
-$_updatedb->updatedb();
-sleep(9);
-$_updatedb->updatedb();
-sleep(9);
-$_updatedb->updatedb();
-sleep(9);
-$_updatedb->updatedb();
-sleep(9);
 $_updatedb->updatedb();
 
 echo "Loading Blocks";
